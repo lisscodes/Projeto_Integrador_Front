3bbfefe5165040f1c880a767b670ea14e3fd94d9
Helen <rutehelenrh@gmail.com>
2022-06-10 10:28:48 -0300

    correções do Put categoria

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-categoria',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = new Categoria()
+  listaTemas: Categoria[]
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    if(environment.token == ''){
+      this.router.navigate(['/login'])
+    }
+    this.findByAllTemas()
   }
+
+  findByAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Categoria[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.categoria).subscribe((resp: Categoria) => {
+      this.categoria = resp 
+      alert('Tema cadastrado com sucesso!')
+      this.findByAllTemas()
+      this.categoria = new Categoria()
+    })
+  }
+
 
 }
--- src/app/edit/categoria-edit/categoria-edit.component.ts
+++ src/app/edit/categoria-edit/categoria-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from 'src/app/model/Categoria';
+import { CategoriaService } from 'src/app/service/categoria.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-categoria-edit',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaEditComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = new Categoria(
 
-  ngOnInit(): void {
-  }
+    )
+    constructor(
+      private categoriaService: CategoriaService,
+      private router: Router,
+      private route: ActivatedRoute
+    ) { }
+  
+    ngOnInit(){
+      window.scroll(0,0)
+
+      if(environment.token == ''){
+        this.router.navigate(['/login'])
+      }
+      let id = this.route.snapshot.params['id']
+      this.findByIdTema(id)
+    }
+  
+    findByIdTema(id: number){
+      this.categoriaService.getByIdTema(id).subscribe((resp: Categoria) => {
+        this.categoria = resp
+      })
+    }
+  
+    atualizar(){
+      this.categoriaService.putTema(this.categoria).subscribe((resp: Categoria) => {
+        this.categoria = resp
+        alert('Tema atualizado com sucesso!')
+        this.router.navigate(['/categoria'])
+      })
+    }
 
 }
